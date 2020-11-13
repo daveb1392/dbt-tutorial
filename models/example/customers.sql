9338955f6d28cdfb87a85f5421785d639a06b849
github username <github email address>
2020-11-13 02:56:08 +0000

    added partition

--- models/example/customers.sql
+++ models/example/customers.sql
@@ -1,10 +1,18 @@
-with customers as (
+{{ config(
+    materialized='table',
+    partition_by={
+      "field": "order_date",
+      "data_type": "timestamp"
+    }
+)}}
 
-    select * from {{ ref('stg_customers') }}
+-- with customers as (
 
-),
+--     select * from {{ ref('stg_customers') }}
 
-orders as (
+-- ),
+
+with orders as (
 
     select * from {{ ref('stg_orders') }}
 
@@ -13,33 +21,31 @@ orders as (
 customer_orders as (
 
     select
-        customer_id,
-
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(distinct order_id) as count_orders,
+        count(distinct customer_id) as users,
+        order_date
 
     from orders
 
-    group by 1
-
-),
-
-
-final as (
-
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
-    from customers
-
-    left join customer_orders using (customer_id)
+    group by 3
 
 )
 
-select * from final
+
+-- final as (
+
+--     select
+--         customers.customer_id,
+--         customers.first_name,
+--         customers.last_name,
+--         customer_orders.first_order_date,
+--         customer_orders.most_recent_order_date,
+--         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+
+--     from customers
+
+--     left join customer_orders using (customer_id)
+
+-- )
+
+select * from customer_orders
--- models/example/stg_orders.sql
+++ models/example/stg_orders.sql
@@ -1,7 +1,12 @@
+{{ config(
+    materialized='table'
+)}}
+
+
 select
     id as order_id,
     user_id as customer_id,
-    order_date,
+    TIMESTAMP(order_date) as order_date,
     status
 
 from `dbt-tutorial`.jaffle_shop.orders
